ec82cf50ea50e86edebed706eb46eac6ebf75147
nikisha-scipt <nikisha_script@mail.ru>
2022-05-26 12:59:52 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,15 +5,20 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements DateTimeParser {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
+        HabrCareerParse habr = new HabrCareerParse();
         Connection connection = Jsoup.connect(PAGE_LINK);
         Document document = connection.get();
         Elements rows = document.select(".vacancy-card__inner");
@@ -26,9 +31,14 @@ public class HabrCareerParse {
             Element dataLink = dataElement.child(0);
             String vacancyName = titleElement.text();
             String vacancyDate = dataElement.text();
-            String link = String.format("%s%s %s", SOURCE_LINK, linkElement.attr("href"), dataLink.attr("datetime"));
+            String link = String.format("%s%s %s", SOURCE_LINK, linkElement.attr("href"), habr.parse(dataLink.attr("datetime")));
             System.out.printf("%s: %s %s%n", vacancyDate, vacancyName, link);
         });
     }
 
+    @Override
+    public LocalDateTime parse(String parse) {
+        Instant timestamp = Instant.parse(parse);
+        return LocalDateTime.ofInstant(timestamp, ZoneOffset.UTC);
+    }
 }
---
+++ src/main/java/ru/job4j/grabber/utils/DateTimeParser.java
@@ -0,0 +1,9 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDateTime;
+
+public interface DateTimeParser {
+
+    LocalDateTime parse(String parse);
+
+}
