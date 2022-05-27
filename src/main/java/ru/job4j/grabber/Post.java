408e7ce383725e42b1c86145433f0b1c8d5eb4c0
nikisha-scipt <nikisha_script@mail.ru>
2022-05-27 14:42:49 +0300

    2.2. Модель данных - Post. [#285211 #305877] (reload)

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -19,6 +19,13 @@ public class Post {
         this.created = created;
     }
 
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
     public int getId() {
         return id;
     }
