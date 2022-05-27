c8863ece6a28f6a7b5c0cc433e70774cd47743bf
nikisha-scipt <nikisha_script@mail.ru>
2022-05-27 12:14:14 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,36 +5,33 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.grabber.utils.DateTimeParser;
-
 import java.io.IOException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 
 public class HabrCareerParse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        HabrCareerParse habr = new HabrCareerParse();
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            assert titleElement != null;
-            Element linkElement = titleElement.child(0);
-            Element dataElement = row.selectFirst(".vacancy-card__date");
-            assert dataElement != null;
-            Element dataLink = dataElement.child(0);
-            String vacancyName = titleElement.text();
-            String vacancyDate = dataElement.text();
-            String link = String.format("%s%s %s", SOURCE_LINK, linkElement.attr("href"), dataLink.attr("datetime"));
-            System.out.printf("%s: %s %s%n", vacancyDate, vacancyName, link);
-
-        });
+        for (int i = 1; i <= 5; i++) {
+            System.out.printf("List card vacancy: %d%n", i);
+            Connection connection = Jsoup.connect(PAGE_LINK + i);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                assert titleElement != null;
+                Element linkElement = titleElement.child(0);
+                Element dataElement = row.selectFirst(".vacancy-card__date");
+                assert dataElement != null;
+                Element dataLink = dataElement.child(0);
+                String vacancyName = titleElement.text();
+                String vacancyDate = dataElement.text();
+                String link = String.format("%s%s %s", SOURCE_LINK, linkElement.attr("href"), dataLink.attr("datetime"));
+                System.out.printf("%s: %s %s%n", vacancyDate, vacancyName, link);
+            });
+        }
     }
+
 
 }
