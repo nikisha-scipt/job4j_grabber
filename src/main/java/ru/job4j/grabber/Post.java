2aa3101aa52ce993d0d668974a21abd7d9401d9d
nikisha-scipt <nikisha_script@mail.ru>
2022-05-27 12:26:15 +0300

    2.2. Модель данных - Post. [#285211] (reload)

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -68,12 +68,12 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id && Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(description, post.description);
+        return id == post.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link, description);
+        return Objects.hash(id);
     }
 
     @Override
